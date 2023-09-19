e508bb36bea3ee516c7441cc5f7a5d5f3413814a
Mark Nottingham <mnot@mnot.net>
2023-09-19 13:24:42 +0100

    tidy

--- httplint/__init__.py
+++ httplint/__init__.py
@@ -1,4 +1,3 @@
-
 from httplint.message import HttpRequest, HttpResponse
 from httplint.note import Notes
 
@@ -9,5 +8,6 @@ __version__ = "0.0.1"
 def lint_request(request: HttpRequest) -> Notes:
     pass
 
-def lint_response(response:HttpResponse, request:HttpRequest=None) -> Notes:
+
+def lint_response(response: HttpResponse, request: HttpRequest = None) -> Notes:
     pass
