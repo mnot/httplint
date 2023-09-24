0f93cba3126059a1d98bac4eb4153d3446d5033d
Mark Nottingham <mnot@mnot.net>
2023-09-24 10:55:54 +1000

    not clear this will be the API.

--- httplint/__init__.py
+++ httplint/__init__.py
@@ -4,10 +4,3 @@ from httplint.note import Notes
 
 __version__ = "0.0.1"
 
-
-def lint_request(request: HttpRequest) -> Notes:
-    pass
-
-
-def lint_response(response: HttpResponse, request: HttpRequest = None) -> Notes:
-    pass
