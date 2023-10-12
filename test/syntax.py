ca48f3da7bee2f944a118eccf48220bc10181b5f
Mark Nottingham <mnot@mnot.net>
2023-10-12 13:27:33 +1100

    count errors
    and exit 1 if there are any

--- test/syntax.py
+++ test/syntax.py
@@ -7,6 +7,7 @@ def check_regex() -> None:
     """Grab all the regex in this module."""
     modules = 0
     count = 0
+    errors = 0
     for module_name in syntax.__all__:
         modules += 1
         full_name = f"httplint.syntax.{module_name}"
@@ -19,9 +20,11 @@ def check_regex() -> None:
                 try:
                     re.compile(attr_value, re.VERBOSE)
                 except re.error as why:
+                    errors += 1
                     print("*", module_name, attr_name, why)
-    print(f"Checked {count} syntax regex in {modules} modules.")
-
+    print(f"Checked {count} syntax regex in {modules} modules; {errors} errors.")
+    if errors > 0:
+        sys.exit(1)
 
 if __name__ == "__main__":
     check_regex()
