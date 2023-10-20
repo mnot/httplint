1e4f4bd5ca2ea341016232f247c1d4382eb2daa3
Mark Nottingham <mnot@mnot.net>
2023-10-20 20:42:07 +1100

    Update Age
    For #28

--- httplint/fields/age.py
+++ httplint/fields/age.py
@@ -1,7 +1,7 @@
 from httplint.fields import HttpField
 from httplint.fields._test import FieldTest
 from httplint.note import Note, categories, levels
-from httplint.syntax import rfc7234
+from httplint.syntax import rfc9111
 from httplint.types import AddNoteMethodType
 from httplint.fields._notes import SINGLE_HEADER_REPEAT
 
@@ -11,8 +11,8 @@ class age(HttpField):
     description = """\
 The `Age` header conveys the sender's estimate of the amount of time since the response (or its
 validation) was generated at the origin server."""
-    reference = f"{rfc7234.SPEC_URL}#header.age"
-    syntax = False  # rfc7234.Age
+    reference = f"{rfc9111.SPEC_URL}#field.age"
+    syntax = False  # rfc9111.Age
     list_header = False
     deprecated = False
     valid_in_requests = False
@@ -27,6 +27,8 @@ validation) was generated at the origin server."""
         if age_value < 0:
             add_note(AGE_NEGATIVE)
             raise ValueError
+        if age_value > 2147483648:
+            add_note(AGE_LARGE)
         return age_value
 
 
@@ -47,6 +49,15 @@ class AGE_NEGATIVE(Note):
 The `Age` header indicates the age of the response; i.e., how long it has been cached
 since it was generated. The value given was negative, so it is not a valid age."""
 
+
+class AGE_LARGE(Note):
+    category = categories.CACHING
+    level = levels.WARN
+    _summary = "The Age header's value is very large."
+    _text = """\
+The `Age` header's value is greater than 2,147,483,648. Some implementations may represent it
+using that value (which is over 68 years).
+"""
 
 class AgeTest(FieldTest):
     name = "Age"
@@ -73,3 +84,9 @@ class NegAgeTest(FieldTest):
     inputs = [b"-20"]
     expected_out = None
     expected_notes = [AGE_NEGATIVE]
+
+class NegAgeTest(FieldTest):
+    name = "Age"
+    inputs = [b"2147483649"]
+    expected_out = 2147483649
+    expected_notes = [AGE_LARGE]
