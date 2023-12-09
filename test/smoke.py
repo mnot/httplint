9ebbff546912a1af27838ff0cf9d9d6986591aa0
Mark Nottingham <mnot@mnot.net>
2023-12-09 11:49:34 +1100

    smoke test a bit more

--- test/smoke.py
+++ test/smoke.py
@@ -7,7 +7,7 @@ linter.process_response_topline(b'HTTP/1.1', b'200', b'OK')
 linter.process_headers([
     (b'Content-Type', b'text/plain'),
     (b'Content-Length', b'10'),
-    (b'Cache-Control', b'max-age=60, max-age=5, max-stale=5')
+    (b'Cache-Control', b'no-store, no-cache, max-age=60, max-age=5, max-stale=5')
 ])
 linter.feed_content(b'12345')
 linter.feed_content(b'67890')
