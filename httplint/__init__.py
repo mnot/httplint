3efbc308a95c4d79a0d9b09b84bd23de57e4ba1b
Mark Nottingham <mnot@mnot.net>
2026-04-10 21:47:05 +1000

    Changed: explicit public API

--- httplint/__init__.py
+++ httplint/__init__.py
@@ -1,5 +1,15 @@
 from httplint.field.description import get_field_description
 from httplint.message import HttpRequestLinter, HttpResponseLinter
-from httplint.note import Notes
+from httplint.note import Note, Notes, categories, levels
 
 __version__ = "2026.04.2"
+
+__all__ = [
+    "HttpRequestLinter",
+    "HttpResponseLinter",
+    "Note",
+    "Notes",
+    "categories",
+    "levels",
+    "get_field_description",
+]
